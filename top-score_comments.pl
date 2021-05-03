7ae182d85a5297900bbd8cfa62812b6ebba2f176
Gustaf Erikson <gerikson@gmail.com>
2021-05-03 22:06:24 +0200

    cleaned up

--- top-score_comments.pl
+++ top-score_comments.pl
@@ -5,6 +5,7 @@ use Getopt::Long;
 
 use HNLOlib qw/$feeds get_ua get_dbh get_reddit get_web_items/;
 use Data::Dump qw/dump/;
+use DateTime;
 use Template;
 use FindBin qw/$Bin/;
 use utf8;
@@ -15,14 +16,19 @@ my $tt = Template->new(
 
 my $data->{meta}->{page_title} = 'Top links by score and comments';
 
+#my $dt_now = Date
+$data->{meta}->{generate_time}
+    = DateTime->now()->strftime('%Y-%m-%d %H:%M:%S%z');
 my %content;
 my @by_score;
 my @by_comments;
 
 for my $label ( 'hn', 'lo', 'pr' ) {
     my $dbh = get_dbh();
+
     #warn "==> getting data for $label... ";
     for my $sorting ( 's', 'c' ) {
+
         #warn "==> sorting = $sorting";
 
         my $statement
@@ -34,18 +40,20 @@ for my $label ( 'hn', 'lo', 'pr' ) {
             ? ' order by score desc '
             : ' order by comments desc ';
         $statement .= ' limit 200';
+
         #say "$statement" . ';';
 
 # . ( $sorting eq 's' ? ' order by score desc ' : ' order by comments desc ' . ' limit 100 ';
         my $list = $dbh->selectall_arrayref($statement);
 
-       $statement
-          = "select date(min(created_time)), date(max(created_time)) from "
-           . $feeds->{$label}->{table_name};
-       my $dates  = $dbh->selectall_arrayref($statement);
-       my $min_ts = $dates->[0]->[0];
-       my $max_ts = $dates->[0]->[1];
-	$data->{dates}->{$label} = { min_ts=>$min_ts, max_ts=>$max_ts};
+        $statement
+            = "select date(min(created_time)), date(max(created_time)) from "
+            . $feeds->{$label}->{table_name};
+        my $dates  = $dbh->selectall_arrayref($statement);
+        my $min_ts = $dates->[0]->[0];
+        my $max_ts = $dates->[0]->[1];
+        $data->{dates}->{$label} = { min_ts => $min_ts, max_ts => $max_ts };
+
         # $statement = "select min(score), max(score) from "
         #     . $feeds->{$label}->{table_name};
         # my $score_range = $dbh->selectall_arrayref($statement);
@@ -56,6 +64,7 @@ for my $label ( 'hn', 'lo', 'pr' ) {
         for my $item (@$list) {
 
             my ( $id, $timestamp, $title, $url, $score, $comments ) = @$item;
+
             # warn "==> ", dump $item unless $score =~ /\d+/;
 
             #    $hist{int (( $score - $min_score ) / $score_width)}++;
@@ -93,7 +102,7 @@ for my $label ( 'hn', 'lo', 'pr' ) {
             # sort by score
             for my $key (
                 sort {
-                    $links{$b}->{score} <=> $links{$a}->{score}
+                           $links{$b}->{score} <=> $links{$a}->{score}
                         || $links{$b}->{comments} <=> $links{$a}->{comments}
                 }
                 keys %links
@@ -110,14 +119,14 @@ for my $label ( 'hn', 'lo', 'pr' ) {
                         qw/timestamp url title score comments/
                     };
 
-		$by_score[$count-1]->{$label} = {
-								                        rank       => $count,
+                $by_score[ $count - 1 ]->{$label} = {
+                    rank       => $count,
                     title_href => $feeds->{$label}->{title_href}
                         . $links{$key}->{id},
                     map { $_ => $links{$key}->{$_} }
                         qw/timestamp url title score comments/
 
-								   };
+                };
                 $count++;
             }
         }
@@ -127,7 +136,7 @@ for my $label ( 'hn', 'lo', 'pr' ) {
             $count = 1;
             for my $key (
                 sort {
-                    $links{$b}->{comments} <=> $links{$a}->{comments}
+                           $links{$b}->{comments} <=> $links{$a}->{comments}
                         || $links{$b}->{score} <=> $links{$a}->{score}
                 }
                 keys %links
@@ -143,13 +152,13 @@ for my $label ( 'hn', 'lo', 'pr' ) {
                     map { $_ => $links{$key}->{$_} }
                         qw/timestamp url title score comments/
                     };
-		$by_comments[$count-1]->{$label} = {
+                $by_comments[ $count - 1 ]->{$label} = {
                     rank       => $count,
                     title_href => $feeds->{$label}->{title_href}
                         . $links{$key}->{id},
                     map { $_ => $links{$key}->{$_} }
                         qw/timestamp url title score comments/
-                    };
+                };
                 $count++;
             }
         }
@@ -162,8 +171,8 @@ for my $label ( 'hn', 'lo', 'pr' ) {
 
 #print dump \%data;
 
-$data->{content} = \%content;
-$data->{by_score} = \@by_score;
+$data->{content}     = \%content;
+$data->{by_score}    = \@by_score;
 $data->{by_comments} = \@by_comments;
 
 #print dump $data;
