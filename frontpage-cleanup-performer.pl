088a34d5aa4fc329b97540cbc6f7ed32745b7ef5
Gustaf Erikson <gerikson@gmail.com>
2023-08-02 21:33:43 +0200

    better error handling

--- frontpage-cleanup-performer.pl
+++ frontpage-cleanup-performer.pl
@@ -9,15 +9,19 @@ use utf8;
 use DateTime::Format::Strptime qw/strftime strptime/;
 use Data::Dump qw/dd/;
 use HNLOlib qw/get_dbh $sql/;
+use Time::HiRes  qw/gettimeofday tv_interval/;
 my $folder = './frontpage-cleanup';
 my $dbh;
-
+my ($total_lines, $total_time) = (0,0);
+my $pause_time=5;
 opendir (my $dir, $folder) or die "can't open $folder for listing: $!";
 my @sqlfiles = grep {/\.sql$/ && -f "$folder/$_"} readdir($dir);
-while (@sqlfiles) {
+my $filecount=1;
+FILE: while (@sqlfiles) {
+    my $t0 = [gettimeofday];
     # pick a file 
     my $file = splice( @sqlfiles, rand @sqlfiles, 1 );
-    say "working on $file";
+    printf( "File #%2d working on %s\n", $filecount, $file);
     open( my $fh, "< $folder/$file") or die "can't open $folder/$file for reading: $!";
     my @lines;
     for (<$fh>) {
@@ -43,15 +47,15 @@ while (@sqlfiles) {
 	    printf("%2d [%2d/%2d] id=%d rank=%2d read_time=%s\n",
 		   $retry, $count, $line_count,$id,$rank,$rt);
 	} else {
-	    printf("[%2d/%2d] id=%d rank=%2d read_time=%s\n",
-		   $count, $line_count, $id,$rank,$rt);
+	    printf("%2s [%2d/%2d] id=%d rank=%2d read_time=%s\n",
+		   ' ',$count, $line_count, $id,$rank,$rt);
 	}
 	$sth->execute();
 	if ($sth->err) {
 	    warn "DBI error: $DBI::err : $DBI::errstr\n";
-	    push @lines, $stmt;
-	    sleep 10;
-	    $retry++;
+	    warn sprintf("sleeping for %d s, then taking next file!\n", $pause_time*12*5);
+	    sleep $pause_time * 12*5;
+	    next FILE; 
 	} else {
 	    $count++;
 	}
@@ -59,9 +63,22 @@ while (@sqlfiles) {
     unlink("$folder/$file") or die "can't unlink $folder/$file: $!";
     say "$file deleted, ", scalar (@sqlfiles), " remaining";
     my $rc = $dbh->disconnect || warn $dbh->errstr;
-    say "sleeping 10s";
-    sleep 10;
+    my $elapsed = tv_interval( $t0);
+    $total_lines += $line_count;
+    $total_time += $elapsed;
+    #    push @times_per_file, [$line_count, $elapsed];
+    say "Lines per second: ". $total_lines/$total_time;
+    my $remaining_lines =0;
 
-
+    for my $f (@sqlfiles) {
+	my ( $lines, $id ) =split(/\-/, $f);
+	$remaining_lines += $lines;
+    }
+    my $processing_time= $remaining_lines/($total_lines/$total_time);
+    $processing_time += $pause_time * scalar( @sqlfiles);
+    say "Time remaining (hours): ", $processing_time/3600;
+    say "sleeping ".$pause_time."s";
+    sleep $pause_time;
+    $filecount++;
 }
 closedir $dir;
