c33f4e24d044f312ad5dda01a6731a49f827e662
Gustaf Erikson <gerikson@gmail.com>
2024-10-31 12:51:35 +0100

    reworked deleted comment report

--- Lo-deleted-comments-report.pl
+++ Lo-deleted-comments-report.pl
@@ -22,86 +22,103 @@ sub convert_to_local {
 }
 
 my $dbh=get_dbh();
-my $comments=$dbh->selectall_arrayref("select lo.id, title, lo.created_time,co.comment_id,co.created_at,co.commenting_user,co.is_deleted,co.is_moderated,co.score,co.flags,co.comment_plain 
-from lobsters lo inner join lo_comments co on lo.id=co.id 
-where (co.is_deleted=1 or co.is_moderated=1) and comment_plain <> 'Comment removed by author'
+my $comments=$dbh->selectall_arrayref("select lo.id, title,lo.url, lo.created_time,co.comment_id,
+co.created_at,co.commenting_user,co.is_deleted,co.is_moderated,co.score,co.flags,co.comment_plain 
+from lobsters lo inner join lo_comments co on lo.id=co.id
 order by lo.created_time desc, co.created_at desc") or warn $dbh->errstr;
 
 
 #my @fields = qw/id title  created_time comment_id created_at commenting_user is_deleted is_moderated comment_plain/;
 my @report;
+my %entries;
 my $curr_id='';
 my $latest_comment = '1900-01-01T00:00:00.000+00:00';
-my $comment_count = 0;
+my $deleted_count = 0;
 #dump $comments;
 for my $row (@$comments) {
-    my ($id,$title,$created_time,$comment_id,$created_at,$commenting_user,$is_deleted,$is_moderated,$score,$flags,$comment_plain)= @$row;
-    if ($id ne $curr_id) {
-	push @report, sprintf("## [%s](%s)\n", $title, $feeds->{lo}{title_href}.$id);
-	push @report, sprintf("*Entry posted on %s*\n", convert_to_local($created_time));
-	$curr_id=$id;
+    my ($id,$title,$url, $created_time,$comment_id,$created_at,$commenting_user,$is_deleted,$is_moderated,$score,$flags,$comment_plain)= @$row;
+    $entries{$id}->{comment_count}++;
+    $entries{$id}->{created_time} = $created_time;
+    $entries{$id}->{title} = $title;
+    $entries{$id}->{url} = $url;
+    if ($entries{$id}->{first_comment}) {
+	$entries{$id}->{first_comment} = $created_at if $created_at lt  $entries{$id}->{first_comment} 
+    } else {
+	$entries{$id}->{first_comment} = '2999-12-31T23:59:59+00:00'
     }
-    my $reason = "deleted" if $is_deleted;
-    $reason .= ' and moderated' if $is_moderated;
+    if ($entries{$id}->{last_comment}) {
+	$entries{$id}->{last_comment} = $created_at if $created_at gt $entries{$id}->{last_comment}
+    } else {
+	$entries{$id}->{last_comment} ='1900-01-01T00:00:00+00:00'
+    }
+
     
-    push @report, sprintf("### [Comment by %s](%s) is %s (score: %d, flags: %d)\n",$commenting_user,$feeds->{lo}{comment_href}.$comment_id,$reason, $score, $flags );
+
+    if ($is_deleted or $is_moderated) {
+	my $reason = "deleted" if $is_deleted;
+	$reason .= ' and moderated' if $is_moderated;
+
+	push @{$entries{$id}->{removed}}, {comment_id=>$comment_id, commenting_user=>$commenting_user, created_at=>convert_to_local($created_at), is_deleted=>$is_deleted,is_moderated=>$is_moderated, score=>$score,flags=>$flags, comment_plain=>$comment_plain, reason=>$reason} unless $comment_plain =~ m/Comment removed by author/;
+	$deleted_count++;
+    }
     
-    push @report, sprintf("*Originally posted on %s*\n", convert_to_local($created_at));
-    push @report, $comment_plain;
-    $comment_count++;
     $latest_comment = $created_at if $created_at gt $latest_comment;
     
 }
 
-#### stats
 
-my $common_sql = 'select lo.id, lo.title,lo.score,lo.comments, co.comment_id, co.commenting_user, co.score, co.flags,is_deleted,is_moderated from lo_comments co inner join lobsters lo on lo.id=co.id';
-
-my @fields = qw/entry_id entry_title entry_score entry_comments comment_id commenting_user comment_score comment_flags is_deleted is_moderated/;
-
-my %results = (1=> { sql => 'order by co.score desc limit 20',
-			      res => undef,
-			    label=> "Top scored comments",},
-	       2 => { sql => 'where flags>1 order by flags desc, co.score,is_deleted desc',
-			      res => undef,
-			    label=>"Most flagged comments",},
-	       3 => {sql=>'where co.score<0 order by co.score',
-				res=>undef,
-			       label=>"Bottom scored comments",},);
-
-
-for my $id (sort keys %results) {
-    my $aryref = $dbh->selectall_arrayref( $common_sql . ' ' . $results{$id}->{sql}) or warn $dbh->errstr;
-    my $res;
-#    say "==> ",$results{$id}->{label}, " <==";
-    for my $r (@$aryref) {
-	my $hashref;
-	for my $p (0..$#fields) {
-	    $hashref->{$fields[$p]} = $r->[$p];
-	}
-	push @$res, $hashref;
-    }
-    $results{$id}->{res} = $res;
-#    dump $res;
-}
-
-
-my $data;
-$data->{meta}->{page_title} = 'Lobste.rs recent comment stats';
-$data->{meta}->{generate_time} = Time::Piece->localtime()->strftime("%FT%T%z");
-$data->{meta}->{latest_comment} = convert_to_local($latest_comment);
-$data->{meta}->{comment_count}= $comment_count;
-$data->{report} = \@report;
-$data->{stats} = \%results;
+my $index;
+$index->{meta}->{page_title} = 'Lobste.rs recent deleted comments';
+$index->{meta}->{generate_time} = Time::Piece->localtime()->strftime("%FT%T%z");
+$index->{meta}->{latest_comment} = convert_to_local($latest_comment);
+$index->{meta}->{deleted_count}= $deleted_count;
+my $pages;
+#$index->{report} = \@report;
+#$index->{stats} = \%results;
 my $tt = Template->new(
     { INCLUDE_PATH => "$Bin/templates", ENCODING => 'UTF-8' } );
 
-$tt->process(
-    'Lo-comments-report.tt', $data
-) || die $tt->error;
+for my $id (sort {$entries{$b}->{created_time} cmp $entries{$a}->{created_time}}keys %entries) {
+    if ( defined $entries{$id}->{removed} ) {
+	push @{$index->{entries}}, {id=>$id, title=>$entries{$id}->{title},
+				    comment_count=>$entries{$id}->{comment_count},
+				    url=> $entries{$id}->{url},
+				    first_comment=>convert_to_local($entries{$id}->{first_comment}),
+				    last_comment=>convert_to_local($entries{$id}->{last_comment}),
+				    deleted_comment_count=> scalar @{$entries{$id}->{removed}}
+				   };
+#	say "Entry $id with title $entries{$id}->{title} has $entries{$id}->{comment_count} comments";
+	# say "Number of deleted or moderated comments: " . scalar @{$entries{$id}->{removed}};
+	for my $comment (@{$entries{$id}->{removed}}) {
+	    
+	    push @{$pages->{$id}{comments}}, {map {$_ => $comment->{$_}} qw/comment_id commenting_user score flags reason created_at comment_plain is_deleted is_moderated/};
+#	    $pages->{$id}{comments}{created_at} = convert_to_local($pages->{$id}{comments}{created_at});
+#	    say join(' ', $comment->{comment_id}, $comment->{commenting_user}, $comment->{score}, $comment->{flags});
+#	    say $comment->{comment_plain};
+	}
+    }  else {
+	next;
+    }
+}
 
+$tt->process(
+    'Lo-deleted-comments-index.tt', $index, 'Deleted/index.html',     { binmode => ':utf8' } ) || die $tt->error;
+
+for my $id (keys %{$pages}) {
+    my $data;
+    $data->{meta}{entry_id} = $id;
+    $data->{meta}->{generate_time} = Time::Piece->localtime()->strftime("%FT%T%z");
+    $data->{meta}{title} = $entries{$id}{title};
+    $data->{comments} = $pages->{$id}{comments};
+
+    $tt->process('Lo-deleted-comments-content.tt', $data, "Deleted/deleted-$id.html", { binmode => ':utf8' })||die $tt->error;
+
+}
 
 __END__
+
+
+
 my %entries;
 for my $row (@$comments) {
     $entries{$row->[0]} = {title=>$row->[1], created_time=>$row->[2]};
